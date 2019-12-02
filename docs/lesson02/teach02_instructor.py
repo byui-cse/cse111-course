bb31b3a45decc618f50f5f291295a3cd79220cb4
Rex Barzee <barzeer@byui.edu>
2019-12-02 14:24:52 -0700

    Add index.html files to aid viewing of content.

--- docs/lesson02/teach02_instructor.py
+++ docs/lesson02/teach02_instructor.py
@@ -6,18 +6,16 @@ inumbers = []
 names = []
 with open("students.csv", "rt") as file:
 	reader = csv.reader(file, delimiter=",")
-	count = 0
 	for row in reader:
-		if count > 0:
+		if reader.line_num > 1:
 			inumbers.append(row[0])
 			names.append(row[1])
-		count += 1
 
 # Get an I-Number from the user.
 text = str(input("Please enter an I-Number (xx-xxx-xxxx): "))
 
 # The I-Numbers are stored in the CSV file as digits only (without any
-# dashes, so we remove all non-digit characters). Also, count the number
+# dashes), so we remove all non-digit characters. Also, count the number
 # of invalid characters.
 ninvalid = 0
 inum = ""
