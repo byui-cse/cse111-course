24f1a27ac79cf242e5c5dd59ac578512b1a77ef1
Rex Barzee <barzeer@byui.edu>
2020-01-21 23:43:56 +0000

    Add a testing procedure to lesson 1 checkpoint.

--- docs/lesson01/check_solution.py
+++ docs/lesson01/check_solution.py
@@ -1,8 +1,8 @@
 # Get the user's age as a string.
 text = input("Please enter your age: ")
 
-# Convert the string that the user entered into an integer.
-age = int(text)
+# Convert the string that the user entered into a positive integer.
+age = abs(round(float(text)))
 
 # Compute the slowest and fastest beneficial heart rates for the user.
 maxRate = 220 - age
@@ -11,6 +11,6 @@ fastest = round(maxRate * 0.85)
 
 # Use f-strings to create a message for the user to see.
 print(
-	f"When you exercise, you should keep your heart\n"
-	f"rate between {slowest} and {fastest} beats per minute."
+	f"When you exercise to strengthen your heart, you should keep\n"
+	f"your heart rate between {slowest} and {fastest} beats per minute."
 )
