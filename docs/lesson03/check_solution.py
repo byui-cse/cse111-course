09fe569b80d78b31e3ef7490b6da206121148fff
Rex Barzee <barzeer@byui.edu>
2020-01-21 20:47:22 +0000

    Add a teach activity to lesson 3

---
+++ docs/lesson03/check_solution.py
@@ -0,0 +1,25 @@
+# Create five variables of different types. Create one each of
+# string, range, point, list, and set.
+message = "What a beautiful day!"
+quant = range(10)
+point = (-2, 1.4, 3)
+names = ["Jason", "Britania", "Anders", "Takuya"]
+colors = {"red", "green", "blue"}
+
+# Call the print function to print the value of each of the variables.
+print(message)
+print(quant)
+print(point)
+print(names)
+print(colors)
+
+# Print a blank line.
+print()
+
+# Call the len function and the print function to print the length of
+# the value of each of the variables.
+print(len(message))
+print(len(quant))
+print(len(point))
+print(len(names))
+print(len(colors))
---
+++ docs/lesson03/teach_solution.py
@@ -0,0 +1,25 @@
+import random
+import textwrap
+
+# Ask the user how many quotes she would like to see.
+quant = int(input("How many quotes would you like? "))
+
+# Open the quotes.txt file.
+with open("quotes.txt", "rt") as infile:
+
+	# Read all the quotes in the file into a list.
+	quotes = infile.readlines()
+
+	for _ in range(quant):
+		# Randomly choose one quote.
+		quote = random.choice(quotes)
+
+		# Wrap the words of the quote if it is a long quote.
+		wrapped = textwrap.wrap(quote, 50)
+
+		# Print a blank line.
+		print()
+
+		# Print the wrapped quote.
+		for line in wrapped:
+			print(line)
