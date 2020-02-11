58b96d3e0d114cb9f6a3c2f92a1289d55560f099
Rex Barzee <barzeer@byui.edu>
2020-02-11 17:07:32 +0000

    Add a prepare checkpoint for lesson 4

--- docs/lesson04/commodities.py
+++ docs/lesson04/commodities.py
@@ -1,22 +1,32 @@
 from datetime import datetime, timedelta
 import requests
 
+# qcode is short for Quandl Code and is the code that determines
+# what data will be downloaded from Quandl by the http request.
 qcode = "ODA/PWOOLC_USD"
 print(qcode)
+
+# This is the uniform resource locator (URL)
+# where this program will send an http request.
 url = f"https://www.quandl.com/api/v3/datasets/{qcode}.json"
 
+# These are the start and end dates for the data request. The start date
+# will be 13 about months before today, and the end date will be today.
 end = datetime.today()
-start = end - timedelta(days=13*31)
+start = end - timedelta(days=13*30)
+
+# Arguments that focus the http request for data.
 args = {
 	"start_date" : start.strftime("%Y-%m-%d"),
 	"end_date" : end.strftime("%Y-%m-%d"),
-	"collaspe" : "daily",
+	"collaspe" : "monthly",
 	"order" : "asc"
 }
 
 # Request the commodities data from Quandl.
 response = requests.get(url, params=args)
 
+# Check the status code to see if the request succeeded.
 if response.status_code == 200:
 	# If the request was succesful, print
 	# the data that Quandl returned to us.
