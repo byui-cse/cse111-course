9309a6868cb61b89360323f32709992db2f644cd
Rex Barzee <BarzeeR@byui.edu>
2020-03-06 21:12:59 +0000

    Improve the teach activity for lesson 7.

--- docs/lesson07/names.py
+++ docs/lesson07/names.py
@@ -1,11 +1,16 @@
 def full_name(given_name, family_name):
-	"""Return a string in this form 'family_name, given_name'"""
+	"""Return a string in this form 'family_name, given_name'.
+	For example, if this function were called like this:
+	full_name("Sally", 'Brown"), it would return "Brown, Sally".
+	"""
 	return family_name + "," + given_name
 
 
 def given_name(full_name):
 	"""Extract and return the given name from a
-	string in this form 'family_name, given_name'
+	string in this form 'family_name, given_name'.
+	For example, if this function were called like this:
+	given_name("Brown, Sally"), it would return "Sally".
 	"""
 	comma = full_name.find(", ")
 	return full_name[comma + 1:]
@@ -13,7 +18,9 @@ def given_name(full_name):
 
 def family_name(full_name):
 	"""Extract and return the family name from a
-	string in this form 'family_name, given_name'
+	string in this form 'family_name, given_name'.
+	For example, if this function were called like this:
+	family_name("Brown, Sally"), it would return "Brown".
 	"""
 	comma = full_name.find(", ")
 	return full_name[0:comma]
