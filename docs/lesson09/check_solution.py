bee7602f862c6a1d9a0ab21cace9e0d8f2d10bee
Rex Barzee <BarzeeR@byui.edu>
2020-03-31 20:00:00 +0100

    Improvements to checkpoint for lesson 9

--- docs/lesson09/check_solution.py
+++ docs/lesson09/check_solution.py
@@ -1,49 +1,26 @@
 import pandas as pd
 
 def main():
-    # Read file
-    data = pd.read_csv("readings.csv",
+    # Read the water.csv file and convert the
+    # readDate column from a string to a datetime64.
+    data = pd.read_csv("water.csv",
             dtype={"meterNumber":"str",
-                "priorReadDate":"str", "readDate":"str",
-                "startRead":"int_", "endRead":"int_",
-                "stubSize":"single", "numberOfDwellings":"int_",
-                "accountNumber":"str", "accountType":"str"
+                "readDate":"str", "numberOfDays":"int_",
+                "reading":"int_", "usage":"int_",
+                "accountType":"str", "numberOfDwellings":"int_"
             },
-            parse_dates=["priorReadDate", "readDate"])
+            parse_dates=["readDate"])
+
+    # Print the data type for each column.
+    print(data.dtypes)
+    print()
+
+    # Print a summary description of the data.
+    print(data.describe())
+    print()
+
+    # Print the data.
     print(data)
-
-    data.sort_values(["accountType", "accountNumber"], inplace=True)
-    print(type(data))
-    print(data)
-
-    print()
-    print(type(data.iloc[3]))
-    print(data.iloc[3])
-    print()
-    print(type(data.loc[3]))
-    print(data.loc[3])
-
-    # Causes an error
-    #print(data.loc["priorReadDate"])
-
-    print()
-    print(type(data["priorReadDate"]))
-    print(data["priorReadDate"])
-
-#    print(data.iloc[3]["priorReadDate"])
-#    print(data.loc[3, "priorReadDate"])
-#    print(data["priorReadDate"][3])
-
-#    data.sort_values(["meterNumber", "readDate"], inplace=True)
-
-    # Print all readings from meter number 1007023238.
-#    meter1007023238 = data.query("meterNumber == '1007023238'")
-#    print(meter1007023238)
-
-    # Print all readings from schools.
-#    print(data.query("accountType == 'School'"))
-
-#    print(sorted(data["accountType"].unique().tolist()))
 
 
 main()
