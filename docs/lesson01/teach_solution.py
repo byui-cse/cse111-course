63656c40690697890bb4846a6f34e4fe5d626a13
Rex Barzee <BarzeeR@byui.edu>
2020-04-23 07:38:14 +0100

    Formatting changes to activities for lesson 1.

--- docs/lesson01/teach_solution.py
+++ docs/lesson01/teach_solution.py
@@ -7,13 +7,13 @@ back and forth once is given by this formula:
           √  9.81
 
 where t is the time in seconds,
-π is PI the ratio of the circumference divided by the diameter of a
-    circle (use math.pi), and
+π is PI the ratio of the circumference divided by
+    the diameter of a circle (use math.pi), and
 n is the length of the pendulum in meters.
 
-Write a program that prompts a user to enter the length of a pendulum in
-meters and then computes and prints the time in seconds that it takes
-for that pendulum to swing back and forth.
+Write a program that prompts a user to enter the length of a
+pendulum in meters and then computes and prints the time in
+seconds that it takes for that pendulum to swing back and forth.
 """
 
 # Import the math module so that
