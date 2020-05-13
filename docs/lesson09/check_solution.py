376ddafefb7cb36124a7ef6064cfa1cda92272b3
Rex Barzee <BarzeeR@byui.edu>
2020-05-13 18:22:55 +0100

    Additions and corrections to lessons 9 and 10.

--- docs/lesson09/check_solution.py
+++ docs/lesson09/check_solution.py
@@ -4,9 +4,8 @@ def main():
     # Read the water.csv file and convert the
     # readDate column from a string to a datetime64.
     data = pd.read_csv("water.csv",
-            dtype={"meterNumber":"str",
-                "readDate":"str", "numberOfDays":"int_",
-                "reading":"int_", "usage":"int_",
+            dtype={"meterNumber":"str", "meterSize":"float32",
+                "readDate":"str", "numberOfDays":"int_", "usage":"int_",
                 "accountType":"str", "numberOfDwellings":"int_"
             },
             parse_dates=["readDate"])
--- docs/lesson10/teach_solution.py
+++ docs/lesson10/teach_solution.py
@@ -1,16 +1,65 @@
-import matplotlib.pyplot as plt
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 def main():
     # Read the water.csv file and convert the
     # readDate column from a string to a datetime64.
     data = pd.read_csv("../lesson09/water.csv",
-            dtype={"meterNumber":"str",
-                "readDate":"str", "numberOfDays":"int_",
-                "reading":"int_", "usage":"int_",
+            dtype={"meterNumber":"str", "meterSize":"float32",
+                "readDate":"str", "numberOfDays":"int_", "usage":"int_",
                 "accountType":"str", "numberOfDwellings":"int_"
             },
             parse_dates=["readDate"])
 
+    # Add to the DataFrame a year column
+    # that contains the year of each reading.
+    data["year"] = data["readDate"].dt.year
 
+    print()
+    show_all_water_usage(data)
+
+    print()
+    show_water_usage_per_dwelling(data)
+
+
+def show_all_water_usage(data):
+    # For each year, compute the total water usage.
+    grouped = data.groupby(["year"])
+    totals = grouped.aggregate(sumUsage=("usage", "sum"))
+
+    print("Total usage for all account types (in 1000 gallons):")
+    print(totals)
+
+
+def show_water_usage_per_dwelling(data):
+    # Filter the data to apartment complexes,
+    # residences, trailer courts, and town homes.
+    classes = ["Apartment Complex", "Residence", "Trailer Court", "Town Homes"]
+    data = data[data["accountType"].isin(classes)]
+
+    # For each meterNumber, compute the total water
+    # usage and the number of dwellings grouped by year.
+    grouped = data.groupby(["meterNumber", "year"])
+    interm = grouped.aggregate(
+                sumUsage=("usage", "sum"),
+                numDwellings=("numberOfDwellings", "mean"))
+
+    # Discard outliers: rows with numDwellings less than 1.
+    interm = interm[interm["numDwellings"] >= 1]
+
+    # For each year, compute the total water
+    # usage and the total number of dwellings.
+    final = interm.groupby("year").aggregate(
+                sumUsage=("sumUsage", "sum"),
+                numDwellings=("numDwellings", "sum"))
+
+    # Add a computed column that is sumUsage / numDwellings.
+    final["usagePerDwelling"] = final["sumUsage"] / final["numDwellings"]
+
+    pd.options.display.float_format = "{:.1f}".format
+    print("Average total usage per dwelling (in 1000 gallons):")
+    print(final)
+
+
+main()
