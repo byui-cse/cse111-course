540da3532314809f05eb20c12609be410dfb9842
Rex Barzee <BarzeeR@byui.edu>
2020-06-17 17:23:31 +0100

    Add comments to the sample Python program for lesson 1.

--- docs/lesson01/sample.py
+++ docs/lesson01/sample.py
@@ -1,7 +1,15 @@
+# Import the sleep function from the time module, so
+# that the sleep function can be used in this program.
 from time import sleep
 
+# Prompt the user to enter her name.
 name = input("Hello! What is your name? ")
+
+# Print the numbers 3, 2, 1.
 for i in range(3, 0, -1):
     print(i, flush=True)
     sleep(0.5)  # 1/2 second
+
+# Use a Python f-string to format a greeting
+# for the user and then print the greeting.
 print(f"Welcome to CSE 111, {name}!")
