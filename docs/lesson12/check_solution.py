1598dcd3afbbf06d0742669493f5407e8f393369
Rex Barzee <BarzeeR@byui.edu>
2020-06-29 16:48:17 +0100

    Improve the prepare content and checkpoint for lesson 12.

--- docs/lesson12/check_solution.py
+++ docs/lesson12/check_solution.py
@@ -1,23 +1,36 @@
 def main():
+    # Create and print a list named fruit.
     fruit = ["pear", "banana", "apple", "mango"]
     print(f"original: {fruit}")
 
+    # Reverse and print the fruit list.
     fruit.reverse()
     print(f"reversed: {fruit}")
 
+    # Append "orange" to the fruit list and print the list.
     fruit.append("orange")
     print(f"append orange: {fruit}")
 
+    # Find where "apple" is located in the fruit list and insert
+    # "cherry" before "apple" in the list and print the list.
     pos = fruit.index("apple")
     fruit.insert(pos, "cherry")
     print(f"insert cherry: {fruit}")
 
+    # Remove "banana" from the fruit list and print the list.
     fruit.remove("banana")
     print(f"remove banana: {fruit}")
 
+    # Pop (remove) the last element from the fruit
+    # list and print the popped element and the list.
+    last = fruit.pop()
+    print(f"pop: {last} {fruit}")
+
+    # Sort and print the fruit list.
     fruit.sort()
     print(f"sorted: {fruit}")
 
+    # Clear and print the fruit list.
     fruit.clear()
     print(f"cleared: {fruit}")
 
