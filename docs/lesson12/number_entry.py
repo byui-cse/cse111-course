1e5fe0279187ac71e7fe489f75e7fbad7b8afb13
Rex Barzee <BarzeeR@byui.edu>
2020-07-07 01:23:57 +0100

    Correct code in IntEntry and FloatEntry.

--- docs/lesson12/number_entry.py
+++ docs/lesson12/number_entry.py
@@ -3,15 +3,19 @@ import tkinter as tk
 
 class IntEntry(tk.Entry):
     """An Entry widget that accepts only
-    integers between an upper and lower bound.
+    integers between a lower and upper bound.
     """
     def __init__(self, parent, lower, upper, **kwargs):
         super().__init__(parent)
+        if lower > 1:
+            lower = 1
+        if upper < -1:
+            upper = -1
         self.lower = lower
         self.upper = upper
-        if not "justify" in kwargs:
+        if "justify" not in kwargs:
             kwargs["justify"] = "right"
-        if not "width" in kwargs:
+        if "width" not in kwargs:
             kwargs["width"] = max(len(str(lower)), len(str(upper)))
         kwargs["validate"] = "key"
         kwargs["validatecommand"] = (parent.register(self.validate), "%P")
@@ -29,22 +33,29 @@ class IntEntry(tk.Entry):
         return valid
 
     def get(self):
+        """Return the integer that the user entered."""
         return int(super().get())
 
     def set(self, n):
+        """Display the integer n for the user to see."""
         self.delete(0, tk.END)
         self.insert(0, str(int(n)))
 
     def clear(self):
+        """Clear the integer, if any, that is in this IntEntry."""
         self.delete(0, tk.END)
 
 
 class FloatEntry(tk.Entry):
     """An Entry widget that accepts only
-    numbers between an upper and lower bound.
+    numbers between a lower and upper bound.
     """
     def __init__(self, parent, lower, upper, **kwargs):
         super().__init__(parent)
+        if lower > 0:
+            lower = 0
+        if upper < 0:
+            upper = 0
         self.lower = lower
         self.upper = upper
         vcmd = (parent.register(self.validate), "%P")
@@ -65,11 +76,14 @@ class FloatEntry(tk.Entry):
         return valid
 
     def get(self):
+        """Return the number that the user entered."""
         return float(super().get())
 
     def set(self, n):
+        """Display the number n for the user to see."""
         self.delete(0, tk.END)
         self.insert(0, str(float(n)))
 
     def clear(self):
+        """Clear the number, if any, that is in this FloatEntry."""
         self.delete(0, tk.END)
