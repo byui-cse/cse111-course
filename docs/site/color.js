0b91143b9446156cd2c075aca388c36334ef5d1f
Rex Barzee <BarzeeR@byui.edu>
2020-07-25 02:51:32 +0100

    Add dark mode.

--- docs/site/color.js
+++ docs/site/color.js
@@ -8,10 +8,10 @@ if (!window.hasOwnProperty('cse111')) {
 cse111.color = {
 	addSchemeHandler : function() {
 		let schemeData = {
-			dark : {remove:'light', add:'dark', symbol:'\u263e',
-					title:'Change to light mode'},
-			light : {remove:'dark', add:'light', symbol:'\u263c',
-					title:'Change to dark mode'}
+			dark : {remove:'light', add:'dark',
+					symbol:'\u263c', title:'Change to light mode'},
+			light : {remove:'dark', add:'light',
+					symbol:'\u263e', title:'Change to dark mode'}
 		};
 
 		let set = function(clist, scheme) {
@@ -43,43 +43,5 @@ cse111.color = {
 		for (let c = 0;  c < ctrls.length;  ++c) {
 			ctrls[c].addEventListener('click', toggle);
 		}
-	},
-
-	addSchemeHandlers : function() {
-		let setScheme = function(off, on) {
-			localStorage.setItem('colorScheme', on);
-			let clist = document.body.classList;
-			clist.remove(off);
-			clist.add(on);
-		};
-		let makeClosure = function(off, on) {
-			return function() { setScheme(off, on); };
-		};
-		let dark = makeClosure('light', 'dark');
-		let light = makeClosure('dark', 'light');
-
-		let ctrls = document.getElementsByClassName('colorCtrls');
-		for (let c = 0;  c < ctrls.length;  ++c) {
-			let spans = ctrls[c].getElementsByTagName('span');
-			for (let s = 0;  s < spans.length;  ++s) {
-				let span = spans[s];
-				let attr = span.getAttribute('name');
-				if (attr == 'dark') {
-					span.addEventListener('click', dark);
-					span.setAttribute('title', 'Dark mode');
-				}
-				else if (attr == 'light') {
-					span.addEventListener('click', light);
-					span.setAttribute('title', 'Light mode');
-				}
-			}
-		}
-
-		let clist = document.body.classList;
-		let scheme = localStorage.getItem('colorScheme');
-		if (!scheme) {
-			scheme = (clist.contains('dark') ? 'dark' : 'light');
-		}
-		clist.add(scheme);
 	}
 };
