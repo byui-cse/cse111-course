62436eb41f64c9882221568ac9eca01f5fcc1bbd
Rex Barzee <BarzeeR@byui.edu>
2020-07-28 05:49:20 +0100

    Minor changes to improve student comprehension.

--- docs/lesson04/grid_points.py
+++ docs/lesson04/grid_points.py
@@ -1,13 +1,16 @@
+# Now that we know you're susceptible to reverse psychology, you may as
+# well spend some time studying this code to understand how it works.  ;-)
+
 import requests
 
 print(
 """Weather forecasts from the U.S. National Weather Service are divided
 into 2.5 km grids. In order to get a forecast, an application must
-specify a desired grid. This program will convert a location in decimal
-latitude and longitude into US NWS grid coordinates.""")
+specify a desired grid. This program will convert a location in
+decimal latitude and longitude into US NWS grid coordinates.""")
 
-lat = float(input("Enter a decimal latitude [-90, 90]: ").strip())
-lng = float(input("Enter a decimal longitude [-180, 180]: ").strip())
+lat = float(input("Enter a decimal latitude [-90.0, 90.0]: ").strip())
+lng = float(input("Enter a decimal longitude [-180.0, 180.0]: ").strip())
 
 # Request the grid points from the U.S. National Weather Service.
 url = f"https://api.weather.gov/points/{lat},{lng}"
@@ -15,8 +18,8 @@ response = requests.get(url)
 
 # Check the status code to see if the request succeeded.
 if response.status_code == 200:
-    # The request succeeded, so get the city,
-    # state, and forecast URL from the response.
+    # The request succeeded, so convert the
+    # response from JSON data into a dictionary.
     data = response.json()["properties"]
 
     # Check to see if the response contains city and
@@ -30,6 +33,7 @@ if response.status_code == 200:
         state = data["relativeLocation"]["properties"]["state"]
         print(f"{city}, {state}")
 
+    # Get the forecast URL from the dictionary.
     forecast_url = data["forecast"]
     print(forecast_url)
 
