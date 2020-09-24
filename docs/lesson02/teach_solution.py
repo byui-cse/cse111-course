1d81dafa7e0eeaf0ad26700898bc0a0ef67e727f
Rex Barzee <barzeer@byui.edu>
2020-09-24 23:46:43 +0100

    Restructure if statements in the teach solution for lesson 2 to make them more understandable to students.

--- docs/lesson02/teach_solution.py
+++ docs/lesson02/teach_solution.py
@@ -10,7 +10,7 @@ with open("students.csv", "rt") as infile:
 
     # Use the csv module to create a reader
     # object that will read from the opened file.
-    reader = csv.reader(infile, delimiter=",")
+    reader = csv.reader(infile)
 
     # The first line of the CSV file contains headings
     # and not a student's I-Number and name, so this
@@ -20,9 +20,9 @@ with open("students.csv", "rt") as infile:
     # Read each row in the CSV file one at a time as a list.
     for row in reader:
 
-		# Get a student's I-Number and name from the current row.
-		inum = row[0]
-		name = row[1]
+        # Get a student's I-Number and name from the current row.
+        inum = row[0]
+        name = row[1]
 
         # Add a student's I-Number and name to the students dictionary.
         students[inum] = name
@@ -36,22 +36,23 @@ inum = str(input("Please enter an I-Number (xx-xxx-xxxx): "))
 inum = inum.replace("-", "")
 
 # Determine if the user input is formatted correctly.
-if inum.isdigit():
-    if len(inum) < 9:
-        print("Invalid I-Number: too few digits")
-    elif len(inum) > 9:
-        print("Invalid I-Number: too many digits")
-    else:
-        # The user input is a valid I-Number. Find
-        # the I-Number in the list of I-Numbers.
-        if inum in students:
-            # Retrieve the student name that corresponds
-            # to the I-Number that the user entered.
-            name = students[inum]
-
-            # Print the student name.
-            print(name)
-        else:
-            print("No such student")
-else:
+if inum.isdigit() == False:
     print("Invalid character in I-Number")
+
+elif len(inum) < 9:
+    print("Invalid I-Number: too few digits")
+
+elif len(inum) > 9:
+    print("Invalid I-Number: too many digits")
+
+elif inum not in students:
+    print("No such student")
+
+else
+    # The user input is a valid I-Number and the I-Number
+    # is in the dictionary. Retrieve the student name that
+    # corresponds to the I-Number that the user entered.
+    name = students[inum]
+
+    # Print the student name.
+    print(name)
