7faf8a8412b4f264b1d4ea828610675bea3f5fa2
Rex Barzee <barzeer@byui.edu>
2020-10-08 00:12:43 +0100

    Move the topic of dictionaries from lesson 2 to lesson 3.

--- docs/lesson02/teach_solution.py
+++ docs/lesson02/teach_solution.py
@@ -1,58 +1,37 @@
-import csv
+filename = "responses.txt"
+print(filename)
 
-# Create an empty dictionary that will store student
-# information with the I-Number number as the key.
-students = {}
+# Create an empty list that will hold the
+# responses that are read from the file.
+responses = []
 
-# Open a file named students.csv and store a reference
-# to the opened file in a variable named infile.
-with open("students.csv", "rt") as infile:
+# Open the responses.txt file.
+with open(filename, "rt") as textfile:
 
-    # Use the csv module to create a reader
-    # object that will read from the opened file.
-    reader = csv.reader(infile)
+    # Each line in the file contains one response which is a number.
+    for line in textfile:
+        # For each line in the file, convert
+        # the text from a string to a number.
+        response = float(line)
 
-    # The first line of the CSV file contains headings
-    # and not a student's I-Number and name, so this
-    # statement skips the first line of the CSV file.
-    next(reader)
+        # Skip all responses that are less than zero.
+        if response >= 0:
+            # Store the number in the responses list.
+            responses.append(response)
 
-    # Read each row in the CSV file one at a time as a list.
-    for row in reader:
+# Compute the quantity, sum, and average of the responses.
+quantity = len(responses)
+total = sum(responses)
+average = total / quantity
 
-        # Get a student's I-Number and name from the current row.
-        inum = row[0]
-        name = row[1]
+# Display the average for the user to see.
+print(f"There are {quantity} responses with an average value of {round(average, 2)}")
 
-        # Add a student's I-Number and name to the students dictionary.
-        students[inum] = name
+# Count how many responses are greater than the average.
+count = 0
+for response in responses:
+    if response > average:
+        count += 1
 
-
-# Get an I-Number from the user.
-inum = str(input("Please enter an I-Number (xx-xxx-xxxx): "))
-
-# The I-Numbers are stored in the CSV file as digits only (without
-# any dashes), so we remove all dashes from the user's input.
-inum = inum.replace("-", "")
-
-# Determine if the user input is formatted correctly.
-if inum.isdigit() == False:
-    print("Invalid character in I-Number")
-
-elif len(inum) < 9:
-    print("Invalid I-Number: too few digits")
-
-elif len(inum) > 9:
-    print("Invalid I-Number: too many digits")
-
-elif inum not in students:
-    print("No such student")
-
-else:
-    # The user input is a valid I-Number and the I-Number
-    # is in the dictionary. Retrieve the student name that
-    # corresponds to the I-Number that the user entered.
-    name = students[inum]
-
-    # Print the student name.
-    print(name)
+# Display the number of responses that are greater than the average.
+print(f"{count} of the responses are greater than the average.")
