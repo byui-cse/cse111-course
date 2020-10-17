0b8274b9a155ad58087d9810b6cae3eec0d85a26
Rex Barzee <barzeer@byui.edu>
2020-10-17 16:56:58 +0100

    Add the statistics standard module to the checkpoint for lesson 4.

--- docs/lesson04/check_solution.py
+++ docs/lesson04/check_solution.py
@@ -1,3 +1,6 @@
+# Import the statistics standard module.
+import statistics
+
 # A list of people's height in meters.
 heights = [
     1.72, 1.50, 1.96, 2.01, 1.75,
@@ -12,12 +15,23 @@ mn = min(heights)
 mx = max(heights)
 s = sum(heights)
 
-# Round the result from the sum function
-# to two digits after the decimal.
+# Call the mean, median, and mode functions and
+# pass the list of heights to each function.
+mean = statistics.mean(heights)
+median = statistics.median(heights)
+mode = statistics.mode(heights)
+
+# Round the result from the sum and mean
+# functions to two digits after the decimal.
 s = round(s, 2)
+mean = round(mean, 2)
 
 # Display the results for the user to see.
 print(f"len: {n}")
 print(f"min: {mn}")
 print(f"max: {mx}")
 print(f"sum: {s}")
+
+print(f"mean: {mean}")
+print(f"median: {median}")
+print(f"mode: {mode}")
