c0a426ec815a2778ba609c953ad5f4ee5afdb72d
Rex Barzee <barzeer@byui.edu>
2020-11-02 20:02:04 +0000

    Simplify the prove assignment for lessons 7 and 8.

--- docs/lesson07/test_chemistry.py
+++ docs/lesson07/test_chemistry.py
@@ -23,6 +23,18 @@ def test_parse():
     pass
 
 
+def test_molar_mass():
+    """Test the chemistry.molar_mass function."""
+    sym_quant_dict = parse_formula("H2O")
+    assert molar_mass(sym_quant_dict) == approx(18.01528)
+
+    sym_quant_dict = parse_formula("C6H6")
+    assert molar_mass(sym_quant_dict) == approx(78.11184)
+
+    sym_quant_dict = parse_formula("PO4H2(CH2)12CH3")
+    assert molar_mass(sym_quant_dict) == approx(280.34072)
+
+
 # Call the main function that is part of pytest so that
 # the test functions in this file will start executing.
 pytest.main(["test_chemistry.py"])
