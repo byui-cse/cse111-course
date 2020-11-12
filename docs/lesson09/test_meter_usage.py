78427651dedf6b3d408bb6248818b6cbd68833be
Rex Barzee <barzeer@byui.edu>
2020-11-12 18:35:55 +0000

    Focus the lesson 9 prove assignment on pandas and move the get_int and insert_after to lesson 10.

--- docs/lesson09/test_meter_usage.py
+++ docs/lesson09/test_meter_usage.py
@@ -1,41 +1,6 @@
-from meter_usage import get_int, insert_after, \
-        add_year_month_column, filter_for_meter, filter_between_dates
-from io import StringIO
+from meter_usage import add_year_month_column, filter_for_meter, filter_between_dates
 import pandas as pd
 import pytest
-import sys
-
-
-def test_get_int(monkeypatch):
-    """Verify that the get_int function does not return a value
-    until the user input is between the minimum and maximum values.
-    """
-    # Setup simulated user input: hi, -8, 10, and 2
-    test_input = StringIO("hi\n-8\n10\n2\n")
-    monkeypatch.setattr(sys, "stdin", test_input)
-
-    assert get_int("Please enter an integer: ", -5, 5) == 2
-
-
-def test_insert_after():
-    """Verify that the insert after function
-    correctly inserts elements into a list.
-    """
-    test_cases = [
-        ([], "b", "a", ["a"]),
-        (["a"], "a", "b", ["a", "b"]),
-        (["a"], "b", "c", ["a", "c"]),
-        (["a", "c"], "a", "b", ["a", "b", "c"]),
-        (["a", "b"], "b", "d", ["a", "b", "d"]),
-        (["a", "b"], "c", "e", ["a", "b", "e"])
-    ]
-    for test_case in test_cases:
-        alist = test_case[0]
-        existing = test_case[1]
-        toinsert = test_case[2]
-        result = test_case[3]
-        assert insert_after(alist, existing, toinsert) == None
-        assert alist == result
 
 
 def test_add_year_month_column():
