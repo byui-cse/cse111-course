e87cf70e85cf5abd16b1ab96031076c4f451ec70
Rex Barzee <barzeer@byui.edu>
2020-11-17 03:18:26 +0000

    Incorporate suggestions from Lee Falin into the prepare content for lessons 4 and 5.

--- docs/lesson05/check_solution.py
+++ docs/lesson05/check_solution.py
@@ -1,58 +1,41 @@
-# Import necessary modules.
-import csv
-
-
 def main():
-    # Open the CSV file fuel_usage.csv
-    with open("fuel_usage.csv", "rt") as usage_file:
+    # Get an odometer value in U.S. miles from the user.
+    start = float(input("Enter the first odometer reading (in miles): "))
 
-        # Use the standard csv module to get
-        # a reader object for the CSV file.
-        reader = csv.reader(usage_file)
+    # Get another odometer value in U.S. miles from the user.
+    end = float(input("Enter the second odometer reading (in miles): "))
 
-        # The first line of the CSV file contains headings
-        # and not fuel usage data, so this statement skips
-        # the first line of the CSV file.
-        next(reader)
+    # Get a fuel amount in U.S. gallons from the user.
+    gallons = float(input("Enter the amount of fuel used (in gallons): "))
 
-        # Print headers for the three columns.
-        print("Date,Miles/Gallon,Liters/100 Km")
+    # Call the miles_per_gallon function and store
+    # the result in a variable named mpg.
+    mpg = miles_per_gallon(start, end, gallons)
 
-        # Process each row in the CSV file.
-        for row in reader:
+    # Call the lp100k_from_mpg function to convert the
+    # miles per gallon to liters per 100 kilometers and
+    # store the result in a variable named lp100k.
+    lp100k = lp100k_from_mpg(mpg)
 
-            # From the current row of the CSV file, get the
-            # date as a string. Don't bother to convert the
-            # date from a string to a date object because the
-            # only thing this program will do with the date is
-            # print it, and a string works great for printing.
-            date = row[0]
+    # Round the miles per gallon to one digit after the decimal.
+    mpg = round(mpg, 1)
 
-            # From the current row of the CSV file, get
-            # starting and ending odometer readings.
-            start = float(row[1])
-            end = float(row[2])
+    # Round the liters per 100 km to two digits after the decimal.
+    lp100k = round(lp100k, 2)
 
-            # From the current row of the CSV file, get
-            # the number of gallons of fuel used.
-            gallons = float(row[3])
-
-            # Call the miles_per_gallon function.
-            mpg = miles_per_gallon(start, end, gallons)
-
-            # Call the lp100k_from_mpg function to convert the
-            # miles per gallon to liters per 100 kilometers.
-            lp100k = lp100k_from_mpg(mpg)
-
-            # Display the results for one row in the csv file.
-            print(date, round(mpg, 1), round(lp100k, 2), sep=",")
+    # Display the results for the user to see.
+    print(f"{mpg} miles per gallon")
+    print(f"{lp100k} liters per 100 kilometers")
 
 
 def miles_per_gallon(start, end, gallons):
     """Compute and return the average number of miles
     that a vehicle traveled per gallon of fuel.
-    start and end are odometer readings in miles.
-    gallons is a fuel amount in U.S. gallons.
+
+    param start: An odometer value in miles.
+    param end: Another odometer value in miles.
+    param gallons: A fuel amount in U.S. gallons.
+    return: Fuel efficiency in miles per gallon.
     """
     mpg = abs(end - start) / gallons
     return mpg
@@ -61,6 +44,9 @@ def miles_per_gallon(start, end, gallons):
 def lp100k_from_mpg(mpg):
     """Convert miles per gallon to liters per 100
     kilometers and return the converted value.
+
+    param mpg: A value in miles per gallon
+    return: The converted value in liters per 100km.
     """
     lp100k = 235.215 / mpg
     return lp100k
