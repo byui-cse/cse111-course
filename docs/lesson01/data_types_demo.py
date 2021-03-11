cc7702ba2482785a0fb077607312db40b28fa516
Rex Barzee <barzeer@byui.edu>
2021-03-11 19:12:06 +0000

    Remove tuple and set from the data_types_demo.py program.

--- docs/lesson01/data_types_demo.py
+++ docs/lesson01/data_types_demo.py
@@ -15,64 +15,69 @@ n = float(input("Please enter another number: "))   # number
 p = m + n                                           # number
 
 # Print the variable names, data types, and values.
-for key, value in {"a":a, "b":b, "c":c, "d":d, "e":e, "f":f, "g":g, "h":h,
-        "i":i, "j":j, "k":k, "m":m, "n":n, "p":p}.items():
-    print(key, type(value), value)
+print(a, type(a), a)
+print(b, type(b), b)
+print(c, type(c), c)
+print(d, type(d), d)
+print(e, type(e), e)
+print(f, type(f), f)
+print(g, type(g), g)
+print(h, type(h), h)
+print(i, type(i), i)
+print(j, type(j), j)
+print(k, type(k), k)
+print(m, type(m), m)
+print(n, type(n), n)
+print(p, type(p), p)
 
 # Print a blank line.
 print()
 
-# Create five variables of different types. Create one
-# each of string, range, tuple, list, set, and dictionary.
-message = "What a beautiful day!"
-quant = range(10)
-point = (-2, 1.4, 3)
+# Demonstrate some of the conversion functions.
+b = bool()
+i = int()
+f = float()
+s = str()
+
+# Print the variable names, data types, length, and values.
+print("bool()", type(b), b)
+print("int()", type(i), i)
+print("float()", type(f), f)
+print("str()", type(s), len(s), s)
+
+# Print a blank line.
+print()
+
+# Demonstrate some of the Python collection types.
+# Create one each of list, and dictionary.
 samples = [2.5, -3.2, 2.1, 0.6, 2.1, 0.7]
-colors = {"red", "green", "blue"}
 students = {
     "67-412-8350" : "Alan Benson",
     "76-240-1835" : "Madison Silverlake",
     "06-412-7583" : "Samyukta Daniels"
 }
 
-# Print the variable names, data types, and values.
-for key, value in {"message":message, "quant":quant, "point":point,
-        "samples":samples, "colors":colors, "students":students}.items():
-    print(key, type(value), len(value), value)
+# Print the variable names, data types, length, and values.
+print("samples", type(samples), len(samples), samples)
+print("students", type(students), len(students), students)
 
 # Print a blank line.
 print()
 
-# Demonstrate some of the Python collection types.
-x = "key"
-y = "value"
-codes = [
-    "''", '""',
-    "()", "(x)", "(x,)",
-    "[]", "[x]", "[x,]",
-    "{}", "{x}", "{x,}",
-    "{}", "{x:y}", "{x:y,}"
-]
-values = [
-    '', "",             # string, string
-    (), (x), (x,),      # tuple, string!, tuple
-    [], [x], [x,],      # list, list, list
-    {}, {x}, {x,},      # dict!, set, set
-    {}, {x:y}, {x:y,}   # dict, dict, dict
-]
-for i, code in enumerate(codes):
-    value = values[i]
-    print(code, type(value), len(value), value)
+# Demonstrate the empty string, empty list, and empty dictionary.
+empty_string1 = ''
+empty_string2 = ""
+empty_string3 = str()
+empty_list1 = []
+empty_list2 = list()
+empty_dict1 = {}
+empty_dict2 = dict()
 
-# Print a blank line.
-print()
-
-# Demonstrate some of the conversion functions.
-print("bool()", type(bool()))
-print("int()", type(int()))
-print("float()", type(float()))
-print("str()", type(str()), len(str()))
-print("tuple()", type(tuple()), len(tuple()))
-print("list()", type(list()), len(list()))
-print("set()", type(set()), len(set()))
-print("dict()", type(dict()), len(dict()))
+# Print the variable names, data types, length, and values.
+print("empty_string1", type(empty_string1), len(empty_string1), empty_string1)
+print("empty_string2", type(empty_string2), len(empty_string2), empty_string2)
+print("empty_string3", type(empty_string3), len(empty_string3), empty_string3)
+print("empty_list1", type(empty_list1), len(empty_list1), empty_list1)
+print("empty_list2", type(empty_list2), len(empty_list2), empty_list2)
+print("empty_dict1", type(empty_dict1), len(empty_dict1), empty_dict1)
+print("empty_dict2", type(empty_dict2), len(empty_dict2), empty_dict2)
