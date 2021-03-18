ec79f5373b54c5d29bb0d7b8d9a9f9eb8082731a
Rex Barzee <barzeer@byui.edu>
2021-03-18 23:55:32 +0100

    Revise the checkpoint for lesson 4 as part of teaching about calling and writing functions earlier in the course.

--- docs/lesson04/check_solution.py
+++ docs/lesson04/check_solution.py
@@ -1,38 +1,41 @@
-# Import the statistics standard module
-# so that it can be used in this program.
-import statistics
+"""Compute and print the volume of a right circular cone."""
 
-# A list of people's height in meters.
-heights = [
-    1.72, 1.50, 1.96, 2.01, 1.75,
-    2.11, 1.60, 1.65, 2.05, 1.50,
-    1.80, 1.83, 2.05, 1.79, 1.84
-]
+# Import the standard math module so that
+# math.pi can be used in this program.
+import math
 
-# Call the len, min, max, and sum functions and
-# pass the list of heights to each function.
-n = len(heights)
-mn = min(heights)
-mx = max(heights)
-s = sum(heights)
 
-# Call the mean, median, and mode functions that are in the
-# statistics module and pass the list of heights to each function.
-mean = statistics.mean(heights)
-median = statistics.median(heights)
-mode = statistics.mode(heights)
+def main():
+    # Call the cone_volume function to compute
+    # the volume of an example cone.
+    ex_radius = 2.8
+    ex_height = 3.2
+    ex_vol = cone_volume(ex_radius, ex_height)
 
-# Round the result from the sum and mean
-# functions to two digits after the decimal.
-s = round(s, 2)
-mean = round(mean, 2)
+    # Print several lines that describe this program.
+    print("This program computes the volume of a right circular cone.")
+    print(f"For example, if the radius of a cone is {ex_radius} and")
+    print(f"the height is {ex_height}, then the volume is {ex_vol}")
+    print()
 
-# Display the results for the user to see.
-print(f"len: {n}")
-print(f"min: {mn}")
-print(f"max: {mx}")
-print(f"sum: {s}")
+    # Get the radius and height of the cone from the user.
+    radius = float(input("Please enter the radius of the cone: "))
+    height = float(input("Please enter the height of the cone: "))
 
-print(f"mean: {mean}")
-print(f"median: {median}")
-print(f"mode: {mode}")
+    # Call the cone_volume function to compute the volume
+    # for the radius and height that came from the user.
+    vol = cone_volume(radius, height)
+    print(f"Radius: {radius}")
+    print(f"Height: {height}")
+    print(f"Volume: {vol}")
+
+
+def cone_volume(radius, height):
+    """Compute and return the volume of a right circular cone."""
+    volume = math.pi * radius**2 * height / 3
+    return volume
+
+
+# Start this program by
+# calling the main function.
+main()
