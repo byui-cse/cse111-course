7ba1a688d3f1954b04d15885c15635edb40f2364
Rex Barzee <barzeer@byui.edu>
2021-03-31 17:35:44 +0100

    Replace the HeartWindow class with a function named create_main_window in the team activity for lesson 12.

--- docs/lesson12/heart_rate.py
+++ docs/lesson12/heart_rate.py
@@ -3,110 +3,107 @@ from number_entry import IntEntry
 
 
 def main():
-    try:
-        # Create the root Tk object.
-        app = tk.Tk()
+    # Create the Tk root object.
+    root = tk.Tk()
 
-        # Create a HeartWindow object which will call
-        # the __init__ function in the HeartWindow class.
-        HeartWindow(app)
+    # Call the create_main_window function which
+    # will create the main window of this program.
+    create_main_window(root)
 
-        # Start the tkinter loop that processes user events
-        # such as key presses and mouse button clicks.
-        app.mainloop()
-
-    except RuntimeError as run_err:
-        print(type(run_err).__name__, run_err, sep=": ")
+    # Start the tkinter loop that processes user events
+    # such as key presses and mouse button clicks.
+    root.mainloop()
 
 
-class HeartWindow(tk.Frame):
-    """The main window of this application."""
+def create_main_window(parent):
+    """Create the main window of this program.
 
-    def __init__(self, parent):
-        """Initialize a Frame object."""
+    Parameter
+        parent: the parent object of the main window
+    Return: nothing
+    """
+    # Create the main window. In tkinter,
+    # a window is also called a frame.
+    frame = tk.Frame(parent)
+    frame.master.title("Heart Rate")
+    frame.pack(padx=4, pady=3, fill=tk.BOTH, expand=1)
 
-        # Call __init__ in the parent class.
-        super().__init__(parent)
+    # Create a label that displays "Age:"
+    lblAge = tk.Label(frame, text="Age:")
 
-        # Create a label that displays "Age:"
-        lblAge = tk.Label(self, text="Age:")
+    # Create a text field where the user will enter her age.
+    txtAge = IntEntry(frame, 1, 90, width=5)
 
-        # Create a text field where the user will enter her age.
-        txtAge = IntEntry(self, 1, 90, width=5)
+    # Create a label that displays "Rates:"
+    lblRates = tk.Label(frame, text="Rates:")
 
-        # Create a label that displays "Rates:"
-        lblRates = tk.Label(self, text="Rates:")
+    # Create labels that will display the results.
+    lblSlow = tk.Label(frame, width=4)
+    lblFast = tk.Label(frame, width=4)
 
-        # Create labels that will display the output.
-        lblSlow = tk.Label(self, width=4)
-        lblFast = tk.Label(self, width=4)
+    # Create the Clear button.
+    btnClear = tk.Button(frame, text="Clear")
 
-        # Create the Clear button.
-        btnClear = tk.Button(self, text="Clear")
-
-        # Layout all the labels, text fields, and buttons in a grid.
-        lblAge.grid(  row=0, column=0, padx=3, pady=2)
-        txtAge.grid(  row=0, column=1, padx=3, pady=2)
-        lblRates.grid(row=0, column=2, padx=(30,3), pady=2)
-        lblSlow.grid( row=0, column=3, padx=3, pady=2)
-        lblFast.grid( row=0, column=4, padx=3, pady=2)
-        btnClear.grid(row=1, column=0, padx=3, pady=2, columnspan=5, sticky="W")
-
-        self.master.title("Heart Rate")
-        self.grid(padx=3, pady=3)
-
-
-        # This function is called each time the user releases a key.
-        def calc(event):
-            """Compute and display the user's slowest
-            and fastest beneficial heart rates.
-            """
-            try:
-                # Get the user's age.
-                age = txtAge.get()
-
-                # Compute the user's maximum heart rate.
-                max = 220 - age
-
-                # Compute the user's slowest and
-                # fastest beneficial heart rates.
-                slow = round(max * 0.65)
-                fast = round(max * 0.85)
-
-                # Display the slowest and fastest benficial
-                # heart rates for the user to see.
-                lblSlow.config(text=str(slow))
-                lblFast.config(text=str(fast))
-
-            except ValueError:
-                # When the user deletes all the digits in the age
-                # text field, clear the slowest and fastest labels.
-                lblSlow.config(text="")
-                lblFast.config(text="")
+    # Layout all the labels, text fields, and buttons in a grid.
+    lblAge.grid(  row=0, column=0, padx=3, pady=3)
+    txtAge.grid(  row=0, column=1, padx=3, pady=3)
+    lblRates.grid(row=0, column=2, padx=(30,3), pady=3)
+    lblSlow.grid( row=0, column=3, padx=3, pady=3)
+    lblFast.grid( row=0, column=4, padx=3, pady=3)
+    btnClear.grid(row=1, column=0, padx=3, pady=3, columnspan=5, sticky="W")
 
 
-        # This function is called each time
-        # the user presses the "Clear" button.
-        def clear():
-            """Clear all the inputs and outputs."""
-            txtAge.delete(0, tk.END)
+    # This function will be called each time the user releases a key.
+    def calc(event):
+        """Compute and display the user's slowest
+        and fastest beneficial heart rates.
+        """
+        try:
+            # Get the user's age.
+            age = txtAge.get()
+
+            # Compute the user's maximum heart rate.
+            max = 220 - age
+
+            # Compute the user's slowest and
+            # fastest beneficial heart rates.
+            slow = round(max * 0.65)
+            fast = round(max * 0.85)
+
+            # Display the slowest and fastest benficial
+            # heart rates for the user to see.
+            lblSlow.config(text=str(slow))
+            lblFast.config(text=str(fast))
+
+        except ValueError:
+            # When the user deletes all the digits in the age
+            # text field, clear the slowest and fastest labels.
             lblSlow.config(text="")
             lblFast.config(text="")
-            txtAge.focus()
 
 
-        # Bind the calc function to the age text field so
-        # that the calc function will be called when the
-        # user changes the text in the text field.
-        txtAge.bind("<KeyRelease>", calc)
-
-        # Bind the clear function to the clear button so
-        # that the clear function will be called when the
-        # user clicks the clear button.
-        btnClear.config(command=clear)
-
-        # Give the keyboard focus to the age text field.
+    # This function will be called each time
+    # the user presses the "Clear" button.
+    def clear():
+        """Clear all the inputs and outputs."""
+        txtAge.delete(0, tk.END)
+        lblSlow.config(text="")
+        lblFast.config(text="")
         txtAge.focus()
+
+
+    # Bind the calc function to the age text field so
+    # that the calc function will be called when the
+    # user changes the text in the text field.
+    txtAge.bind("<KeyRelease>", calc)
+
+    # Bind the clear function to the clear button so
+    # that the clear function will be called when the
+    # user clicks the clear button.
+    btnClear.config(command=clear)
+
+    # Give the keyboard focus to the age text field.
+    txtAge.focus()
 
 
 # If this file is executed like this:
