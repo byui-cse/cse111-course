fcaa64278e3fa77dbb31a793047e1594ccec616e
Rex Barzee <barzeer@byui.edu>
2021-03-31 09:13:28 +0100

    Revise content for lessons 1-4 and 11-12 as part of teaching about calling and writing functions earlier in the course.

--- docs/lesson04/teach_stretch.py
+++ docs/lesson04/teach_stretch.py
@@ -19,45 +19,70 @@ def main():
         ["#303", 8.1, 11.11, 0.42]
     ]
 
-    max_store_eff = -1
-    max_cost_eff = -1
     best_store = None
     best_cost = None
+    max_store_eff = -1
+    max_cost_eff = -1
 
+    # For each can in the can_sizes list, unpack the values
+    # into the variables name, radius, height, and cost.
     for name, radius, height, cost in can_sizes:
+
+        # Call the storage_efficiency and cost_efficiency functions.
         store_eff = storage_efficiency(radius, height)
         cost_eff =  cost_efficiency(radius, height, cost)
+
+        # Print the can size name, storage
+        # efficiency, and cost efficiency.
         print(name, store_eff, cost_eff)
 
+        # If the storage efficiency of the current can size is
+        # greater than the maximum storage efficiency, save then
+        # the current can size name and its storage efficiency.
         if store_eff > max_store_eff:
-            max_store_eff = store_eff
             best_store = name
+            max_store_eff = store_eff
 
+        # If the cost efficiency of the current can size is
+        # greater than the maximum cost efficiency, then save
+        # the current can size name and its cost efficiency.
         if cost_eff > max_cost_eff:
-            max_cost_eff = cost_eff
             best_cost = name
+            max_cost_eff = cost_eff
 
+    # Print the best storage and cost efficiencies.
     print("Best can size in storage efficiency:", best_store)
     print("Best can size in cost efficiency:", best_cost)
-
-
-def cost_efficiency(radius, height, cost):
-    """Compute and return the cost efficiency of a steel can size.
-    The cost efficiency is the volume of the can divided by its cost.
-    """
-    volume = cylinder_volume(radius, height)
-    efficiency = round(volume / cost, 1)
-    return efficiency
 
 
 def storage_efficiency(radius, height):
     """Compute and return the storage efficiency of a steel can size.
     The storage efficiency is the volume of the can divided by its
     surface area.
+
+    Parameters
+        radius: the radius of the steel can
+        height: the height of the steel can
+    Return: the storage efficiency of the steel can
     """
     volume = cylinder_volume(radius, height)
     surf_area = cylinder_surface_area(radius, height)
     efficiency = round(volume / surf_area, 1)
+    return efficiency
+
+
+def cost_efficiency(radius, height, cost):
+    """Compute and return the cost efficiency of a steel can size.
+    The cost efficiency is the volume of the can divided by its cost.
+
+    Parameters
+        radius: the radius of the steel can
+        height: the height of the steel can
+        cost: the cost of the steel can
+    Return: the cost efficiency of the steel can
+    """
+    volume = cylinder_volume(radius, height)
+    efficiency = round(volume / cost, 1)
     return efficiency
 
 
