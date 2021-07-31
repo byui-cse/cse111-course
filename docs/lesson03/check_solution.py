eba8ba3fcf2a78893a4988a4b5c228c12d80812a
Rex Barzee <barzeer@byui.edu>
2021-07-31 03:11:41 +0100

    Add "console" divs to show the output of example code.

--- docs/lesson03/check_solution.py
+++ docs/lesson03/check_solution.py
@@ -46,7 +46,6 @@ def main():
 def miles_per_gallon(start_miles, end_miles, amount_gallons):
     """Compute and return the average number of miles
     that a vehicle traveled per gallon of fuel.
-
     Parameters
         start_miles: An odometer value in miles.
         end_miles: Another odometer value in miles.
@@ -60,7 +59,6 @@ def miles_per_gallon(start_miles, end_miles, amount_gallons):
 def lp100k_from_mpg(mpg):
     """Convert miles per gallon to liters per 100
     kilometers and return the converted value.
-
     Parameter mpg: A value in miles per gallon
     Return: The converted value in liters per 100km.
     """
