18551ac426468b32bcc9ee582f82c8c31f392bd8
Rex Barzee <barzeer@byui.edu>
2021-10-26 22:46:00 +0100

    Improve readability of the checkpoint and team activity for lesson 4.

--- docs/lesson04/teach_solution.py
+++ docs/lesson04/teach_solution.py
@@ -10,101 +10,101 @@ def main():
     name = "#1 Picnic"
     radius = 6.83
     height = 10.16
-    volume = cylinder_volume(radius, height)
-    surf_area = cylinder_surface_area(radius, height)
+    volume = compute_volume(radius, height)
+    surf_area = compute_surface_area(radius, height)
     storage_efficiency = volume / surf_area
     print(f"{name} {storage_efficiency:.1f}")
 
     name = "#1 Tall"
     radius = 7.78
     height = 11.91
-    volume = cylinder_volume(radius, height)
-    surf_area = cylinder_surface_area(radius, height)
+    volume = compute_volume(radius, height)
+    surf_area = compute_surface_area(radius, height)
     storage_efficiency = volume / surf_area
     print(f"{name} {storage_efficiency:.1f}")
 
     name = "#2"
     radius = 8.73
     height = 11.59
-    volume = cylinder_volume(radius, height)
-    surf_area = cylinder_surface_area(radius, height)
+    volume = compute_volume(radius, height)
+    surf_area = compute_surface_area(radius, height)
     storage_efficiency = volume / surf_area
     print(f"{name} {storage_efficiency:.1f}")
 
     name = "#2.5"
     radius = 10.32
     height = 11.91
-    volume = cylinder_volume(radius, height)
-    surf_area = cylinder_surface_area(radius, height)
+    volume = compute_volume(radius, height)
+    surf_area = compute_surface_area(radius, height)
     storage_efficiency = volume / surf_area
     print(f"{name} {storage_efficiency:.1f}")
 
     name = "#3 Cylinder"
     radius = 10.79
     height = 17.78
-    volume = cylinder_volume(radius, height)
-    surf_area = cylinder_surface_area(radius, height)
+    volume = compute_volume(radius, height)
+    surf_area = compute_surface_area(radius, height)
     storage_efficiency = volume / surf_area
     print(f"{name} {storage_efficiency:.1f}")
 
     name = "#5"
     radius = 13.02
     height = 14.29
-    volume = cylinder_volume(radius, height)
-    surf_area = cylinder_surface_area(radius, height)
+    volume = compute_volume(radius, height)
+    surf_area = compute_surface_area(radius, height)
     storage_efficiency = volume / surf_area
     print(f"{name} {storage_efficiency:.1f}")
 
     name = "#6Z"
     radius = 5.4
     height = 8.89
-    volume = cylinder_volume(radius, height)
-    surf_area = cylinder_surface_area(radius, height)
+    volume = compute_volume(radius, height)
+    surf_area = compute_surface_area(radius, height)
     storage_efficiency = volume / surf_area
     print(f"{name} {storage_efficiency:.1f}")
 
     name = "#8Z short"
     radius = 6.83
     height = 7.62
-    volume = cylinder_volume(radius, height)
-    surf_area = cylinder_surface_area(radius, height)
+    volume = compute_volume(radius, height)
+    surf_area = compute_surface_area(radius, height)
     storage_efficiency = volume / surf_area
     print(f"{name} {storage_efficiency:.1f}")
 
     name = "#10"
     radius = 15.72
     height = 17.78
-    volume = cylinder_volume(radius, height)
-    surf_area = cylinder_surface_area(radius, height)
+    volume = compute_volume(radius, height)
+    surf_area = compute_surface_area(radius, height)
     storage_efficiency = volume / surf_area
     print(f"{name} {storage_efficiency:.1f}")
 
     name = "#211"
     radius = 6.83
     height = 12.38
-    volume = cylinder_volume(radius, height)
-    surf_area = cylinder_surface_area(radius, height)
+    volume = compute_volume(radius, height)
+    surf_area = compute_surface_area(radius, height)
     storage_efficiency = volume / surf_area
     print(f"{name} {storage_efficiency:.1f}")
 
     name = "#300"
     radius = 7.62
     height = 11.27
-    volume = cylinder_volume(radius, height)
-    surf_area = cylinder_surface_area(radius, height)
+    volume = compute_volume(radius, height)
+    surf_area = compute_surface_area(radius, height)
     storage_efficiency = volume / surf_area
     print(f"{name} {storage_efficiency:.1f}")
 
     name = "#303"
     radius = 8.1
     height = 11.11
-    volume = cylinder_volume(radius, height)
-    surf_area = cylinder_surface_area(radius, height)
+    volume = compute_volume(radius, height)
+    surf_area = compute_surface_area(radius, height)
     storage_efficiency = volume / surf_area
     print(f"{name} {storage_efficiency:.1f}")
 
 
-def cylinder_volume(radius, height):
+def compute_volume(radius, height):
     """Compute and return the volume of a cylinder.
 
     Parameters
@@ -116,7 +116,7 @@ def cylinder_volume(radius, height):
     return volume
 
 
-def cylinder_surface_area(radius, height):
+def compute_surface_area(radius, height):
     """Compute and return the surface area of a cylinder.
 
     Parameters
--- docs/lesson04/teach_stretch.py
+++ docs/lesson04/teach_stretch.py
@@ -31,9 +31,10 @@ def main():
     # into the variables name, radius, height, and cost.
     for name, radius, height, cost in can_sizes:
 
-        # Call the storage_efficiency and cost_efficiency functions.
-        store_eff = storage_efficiency(radius, height)
-        cost_eff =  cost_efficiency(radius, height, cost)
+        # Call the compute_storage_efficiency and
+        # compute_cost_efficiency functions.
+        store_eff = compute_storage_efficiency(radius, height)
+        cost_eff  = compute_cost_efficiency(radius, height, cost)
 
         # Print the can size name, storage
         # efficiency, and cost efficiency.
@@ -58,7 +59,7 @@ def main():
     print("Best can size in cost efficiency:", best_cost)
 
 
-def storage_efficiency(radius, height):
+def compute_storage_efficiency(radius, height):
     """Compute and return the storage efficiency of a steel can size.
     The storage efficiency is the volume of the can divided by its
     surface area.
@@ -68,13 +69,13 @@ def storage_efficiency(radius, height):
         height: the height of the steel can
     Return: the storage efficiency of the steel can
     """
-    volume = cylinder_volume(radius, height)
-    surf_area = cylinder_surface_area(radius, height)
+    volume = compute_volume(radius, height)
+    surf_area = compute_surface_area(radius, height)
     efficiency = volume / surf_area
     return efficiency
 
 
-def cost_efficiency(radius, height, cost):
+def compute_cost_efficiency(radius, height, cost):
     """Compute and return the cost efficiency of a steel can size.
     The cost efficiency is the volume of the can divided by its cost.
 
@@ -84,12 +85,12 @@ def cost_efficiency(radius, height, cost):
         cost: the cost of the steel can
     Return: the cost efficiency of the steel can
     """
-    volume = cylinder_volume(radius, height)
+    volume = compute_volume(radius, height)
     efficiency = volume / cost
     return efficiency
 
 
-def cylinder_volume(radius, height):
+def compute_volume(radius, height):
     """Compute and return the volume of a cylinder.
 
     Parameters
@@ -101,7 +102,7 @@ def cylinder_volume(radius, height):
     return volume
 
 
-def cylinder_surface_area(radius, height):
+def compute_surface_area(radius, height):
     """Compute and return the surface area of a cylinder.
 
     Parameters
