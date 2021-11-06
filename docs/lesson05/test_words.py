2118a763574d69fd8380d870d73ed708b1659452
Rex Barzee <barzeer@byui.edu>
2021-11-06 01:40:20 +0100

    Improve content for lesson 5 by adding introductions and summaries. In other words, tell the students what you will tell them, then tell them, then tell them what you told them.

---
+++ docs/lesson05/test_words.py
@@ -0,0 +1,25 @@
+"""Verify that the prefix and suffix functions work correctly."""
+
+from words import prefix, suffix
+import pytest
+
+
+def test_prefix():
+    """Verify that the prefix function works correctly.
+    Parameters: none
+    Return: nothing
+    """
+    assert prefix("", "") == ""
+    assert prefix("", "correct") == ""
+    assert prefix("clear", "") == ""
+    assert prefix("happy", "funny") == ""
+    assert prefix("cat", "catalog") == "cat"
+    assert prefix("dogmatic", "dog") == "dog"
+    assert prefix("jump", "joyous") == "j"
+    assert prefix("unwise", "ungrateful") == "un"
+    assert prefix("Disable", "dIstasteful") == "dis"
+
+
+# Call the main function that is part of pytest so that
+# the test functions in this file will start executing.
+pytest.main(["-v", "--tb=line", "-rN", __file__])
