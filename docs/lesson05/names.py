d34566b6abdcfd2a9a6a71a3df3b7a362946ceee
Rex Barzee <barzeer@byui.edu>
2022-01-01 23:05:06 +0000

    Changes from proofreading of printed version.

--- docs/lesson05/names.py
+++ docs/lesson05/names.py
@@ -4,16 +4,25 @@ def make_full_name(given_name, family_name):
     """Return a string in this form "family_name; given_name". For
     example, if this function were called like this:
     make_full_name("Sally", "Brown"), it would return "Brown; Sally".
+
+    Parameters
+        given_name: a string that contains a person's given name
+        family_name: a string that contains a person's family name
+    Return: a string in the form "family_name; given_name"
     """
     full_name = f"{family_name};{given_name}"
     return full_name
 
 
 def extract_family_name(full_name):
-    """Extract and return the family name from a string in this form
+    """Extract and return the family name from a string in this form:
     "family_name; given_name". For example, if this function were
     called like this:
     extract_family_name("Brown; Sally"), it would return "Brown".
+
+    Parameter:
+        full_name: a string in the form "family_name; given_name"
+    Return: a string that contains a person's family name
     """
     # Find the index where "; " appears within the full name string.
     semicolon_index = full_name.index("; ")
@@ -24,10 +33,14 @@ def extract_family_name(full_name):
 
 
 def extract_given_name(full_name):
-    """Extract and return the given name from a string in this form
+    """Extract and return the given name from a string in this form:
     "family_name; given_name". For example, if this function were
     called like this:
     extract_given_name("Brown; Sally"), it would return "Sally".
+
+    Parameter:
+        full_name: a string in the form "family_name; given_name"
+    Return: a string that contains a person's given name
     """
     # Find the index where "; " appears within the full name string.
     semicolon_index = full_name.index("/ ")
